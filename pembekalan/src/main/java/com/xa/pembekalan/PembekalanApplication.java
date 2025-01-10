45d4e9db0c09575cbab88eb6a254a9c7d9cb8456
Matthew George Wellson <102170142+Kaisarming@users.noreply.github.com>
2025-01-10 16:41:55 +0700

    Pembekalan Java Day 5

---
+++ pembekalan/src/main/java/com/xa/pembekalan/PembekalanApplication.java
@@ -0,0 +1,13 @@
+package com.xa.pembekalan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PembekalanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PembekalanApplication.class, args);
+	}
+
+}
---
+++ pembekalan/src/test/java/com/xa/pembekalan/PembekalanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xa.pembekalan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PembekalanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
