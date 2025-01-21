a7375fc0617b82912e6cfa03bef89c7b63062173
Matthew George Wellson <102170142+Kaisarming@users.noreply.github.com>
2025-01-21 20:37:57 +0700

    Save Progress Pembekalan Java Day 12 Malam

--- pembekalan/src/main/java/com/xa/pembekalan/controller/BookController.java
+++ pembekalan/src/main/java/com/xa/pembekalan/controller/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.xa.pembekalan.config.ApiEndpoints;
 import com.xa.pembekalan.dto.response.AuthorResponseDto;
+import com.xa.pembekalan.dto.response.BookResponseDto;
 import com.xa.pembekalan.dto.response.PublisherResponseDto;
 import com.xa.pembekalan.entity.Book;
 import com.xa.pembekalan.entity.Category;
@@ -20,6 +21,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.xa.pembekalan.service.AuthorService;
+import com.xa.pembekalan.service.BookService;
 import com.xa.pembekalan.service.PublisherService;
 
 @Controller
@@ -34,6 +36,9 @@ public class BookController {
 
     @Autowired
     PublisherService publisherService;
+
+    @Autowired
+    BookService bookService;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
@@ -50,13 +55,17 @@ public class BookController {
     public ModelAndView showForm() {
         ModelAndView view = new ModelAndView("book/form");
         Book book = new Book();
-        List<AuthorResponseDto> authorResponseDtos = authorService.getAllAuthors();
-        List<Category> categories = categoryRepository.findAll();
-        List<PublisherResponseDto> publisherResponseDtos = publisherService.getAllPublishers();
-        view.addObject("authors", authorResponseDtos);
-        view.addObject("categories", categories);
-        view.addObject("publishers", publisherResponseDtos);
         view.addObject("book", book);
+
+        List<AuthorResponseDto> authors = authorService.getAllAuthors();
+        view.addObject("authors", authors);
+
+        List<Category> categories = categoryRepository.findAll();
+        view.addObject("categories", categories);
+
+        List<PublisherResponseDto> publishers = publisherService.getAllPublishers();
+        view.addObject("publishers", publishers);
+
         return view;
     }
 
